4d1c12bab366db87ed6f39a49fabdfff3b4ac080
W. Meijer <houterm@gmail.com>
2024-08-30 23:36:34 +0200

    fix: cleanup main

--- src/humid/main.py
+++ src/humid/main.py
@@ -1,5 +1,7 @@
 import random
 
+from humid import helpers
+
 from .words.age import AGES
 from .words.color import COLORS
 from .words.material import MATERIALS
@@ -37,39 +39,12 @@ ORDER = [
 ]
 
 
-def calc_probability_of_duplicate():
-    lengths = []
-    odds = 1
-    for list in ORDER:
-        lengths.append(len(list))
-
-    for length in lengths:
-        odds *= 1 / length
-
-    return odds
-
-
-def calc_1_in_XXXXX_chance_of_duplicate():
-    lengths = []
-    odds = 1
-    for list in ORDER:
-        lengths.append(len(list))
-
-    for length in lengths:
-        odds *= 1 / length
-
-    one_in_x = 1 / odds
-    one_in_x_rounded = int(round(one_in_x))
-
-    return one_in_x_rounded
-
-
 def generate() -> str:
     """Returns a human readable identifier."""
     SEP = "-"
     print(
         # f"The probability of intersection = {calc_probability_of_duplicate() * 100:.2e}%"
-        f"The probability of intersection is 1 in {"{:,}".format(calc_1_in_XXXXX_chance_of_duplicate()).replace(",", ".")}"
+        f"The probability of intersection is 1 in {"{:,}".format(helpers.calc_1_in_XXXXX_chance_of_duplicate(ORDER)).replace(",", ".")}"
     )
 
     selection: list[str] = []
