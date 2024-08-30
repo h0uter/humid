e932e9a41dbfab9f72d5740cb1596e240d823af0
W. Meijer <houterm@gmail.com>
2024-08-30 19:42:29 +0200

    add the basics

---
+++ examples/basics.py
@@ -0,0 +1,3 @@
+from humid import human_id
+
+print(human_id())
--- src/humid/main.py
+++ src/humid/main.py
@@ -7,5 +7,10 @@ WORDS = [
 ]
 
 
-def human_id():
-    return random.choice(WORDS)
+def human_id() -> str:
+    """Returns a human readable identifier."""
+    SEP = "-"
+    my_id = (
+        random.choice(WORDS) + SEP + random.choice(WORDS) + SEP + random.choice(WORDS)
+    )
+    return my_id
