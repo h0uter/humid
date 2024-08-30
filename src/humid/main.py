db511bbe13221002d4773c8659704353dba595a3
W. Meijer <houterm@gmail.com>
2024-08-30 22:02:52 +0200

    feat: add chance of intersection.

--- src/humid/main.py
+++ src/humid/main.py
@@ -37,9 +37,40 @@ ORDER = [
 ]
 
 
+def calc_probability_of_duplicate():
+    lengths = []
+    odds = 1
+    for list in ORDER:
+        lengths.append(len(list))
+
+    for length in lengths:
+        odds *= 1 / length
+
+    return odds
+
+
+def calc_1_in_XXXXX_chance_of_duplicate():
+    lengths = []
+    odds = 1
+    for list in ORDER:
+        lengths.append(len(list))
+
+    for length in lengths:
+        odds *= 1 / length
+
+    one_in_x = 1 / odds
+    one_in_x_rounded = int(round(one_in_x))
+
+    return one_in_x_rounded
+
+
 def generate() -> str:
     """Returns a human readable identifier."""
     SEP = "-"
+    print(
+        # f"The probability of intersection = {calc_probability_of_duplicate() * 100:.2e}%"
+        f"The probability of intersection is 1 in {"{:,}".format(calc_1_in_XXXXX_chance_of_duplicate()).replace(",", ".")}"
+    )
 
     selection: list[str] = []
     for words_to_choose_from in ORDER:
