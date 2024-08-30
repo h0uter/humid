5b09b9f5db46b9780c99331a9eedda28f47425c2
W. Meijer <houterm@gmail.com>
2024-08-30 23:35:34 +0200

    test: check maximum collision chance.

---
+++ src/humid/helpers.py
@@ -0,0 +1,25 @@
+def calc_probability_of_duplicate(list_lists_of_words: list[list[str]]):
+    lengths = []
+    odds = 1
+    for list in list_lists_of_words:
+        lengths.append(len(list))
+
+    for length in lengths:
+        odds *= 1 / length
+
+    return odds
+
+
+def calc_1_in_XXXXX_chance_of_duplicate(list_lists_of_words: list[list[str]]):
+    lengths = []
+    odds = 1
+    for list in list_lists_of_words:
+        lengths.append(len(list))
+
+    for length in lengths:
+        odds *= 1 / length
+
+    one_in_x = 1 / odds
+    one_in_x_rounded = int(round(one_in_x))
+
+    return one_in_x_rounded
---
+++ tests/test_probabilty.py
@@ -0,0 +1,12 @@
+from humid import helpers
+from humid.main import ORDER
+
+# def maximum_chance_of_collision():
+#     duplicate_probability = helpers.calc_probability_of_duplicate(ORDER)
+#     assert duplicate_probability <
+
+
+def test_maximum_1_in_XXXXX_chance_of_collision():
+    XXXXX = 131_562_000_000  # prob at 30-08-2024
+    duplicate_probability = helpers.calc_1_in_XXXXX_chance_of_duplicate(ORDER)
+    assert duplicate_probability > XXXXX
