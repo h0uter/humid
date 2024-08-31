3756801080463bb8435756548dbbf855037b8279
W. Meijer <houterm@gmail.com>
2024-08-31 15:18:46 +0200

    fix: cleanup

--- tests/test_probabilty.py
+++ tests/test_probabilty.py
@@ -1,10 +1,6 @@
 from humid import helpers
 from humid.core import ORDER
 
-# def maximum_chance_of_collision():
-#     duplicate_probability = helpers.calc_probability_of_duplicate(ORDER)
-#     assert duplicate_probability <
-
 
 def test_maximum_1_in_XXXXX_chance_of_collision():
     XXXXX = 131_562_000_000  # prob at 30-08-2024
