2a40569d3eeb4bebae6c090a586b901246359966
W. Meijer <wouter.meijer@tno.nl>
2024-09-06 21:39:21 +0200

    feat: add basic cli skeleton.

--- src/humid/__main__.py
+++ src/humid/__main__.py
@@ -1,5 +1,13 @@
+import argparse
+
 from humid import hrid
 
 
 def main():
+    parser = argparse.ArgumentParser(description="humid generates human readable ids.")
+    # parser.add_argument("--foo", type=str, help="A sample argument for foo")
+    # parser.add_argument("--bar", type=int, help="A sample argument for bar")
+
+    parser.parse_args()
+
     print(hrid())
