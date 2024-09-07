0309132e3a1fe4c173a6ce92b5e7139573bc2970
W. Meijer <wouter.meijer@tno.nl>
2024-09-07 13:19:35 +0200

    test: add test that checks if there are spaces in any of the words in our dictionary.

--- tests/test_words.py
+++ tests/test_words.py
@@ -20,3 +20,19 @@ def test_no_duplicates_between():
     duplicates = [item for item, count in Counter(all_words).items() if count > 1]
     print(f"{duplicates=}")
     assert len(all_words) == len(set(all_words))
+
+
+def test_no_spaces_in_words():
+    """Test whether there are undesired spaces in any of the words."""
+    all_words = sum(ORDER, [])
+    words_with_spaces = []
+
+    for word in all_words:
+        if " " in word:
+            words_with_spaces.append(word)
+
+    print(f"{words_with_spaces=}")
+    assert len(words_with_spaces) == 0
+
+
+test_no_spaces_in_words()
