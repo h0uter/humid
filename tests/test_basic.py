68ceefd685402f317b21f07875954371e6d4f693
W. Meijer <wouter.meijer@tno.nl>
2024-09-08 14:43:01 +0200

    fix: move collision test to correct file.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -7,17 +7,3 @@ def test_basic():
     """Test whether a hrid is outputted of the correct type."""
     my_id = hrid()
     assert isinstance(my_id, str)
-
-
-def test_no_duplicate_id():
-    """Test whether no collisions are encountered when generating N times."""
-    TIMES_TO_GENERATE = 1_000
-    ids = set()
-
-    for i in range(TIMES_TO_GENERATE):
-        the_id = hrid()
-
-        if the_id in ids:
-            raise ValueError(f"iter {i}: {the_id} already present in set")
-
-        ids.add(the_id)
--- tests/test_collision.py
+++ tests/test_collision.py
@@ -1,1 +1,17 @@
 """These tests validate probability of generating colliding hrids."""
+
+from humid import hrid
+
+
+def test_no_duplicate_id():
+    """Test whether no collisions are encountered when generating N times."""
+    TIMES_TO_GENERATE = 1_000_000
+    ids = set()
+
+    for i in range(TIMES_TO_GENERATE):
+        the_id = hrid()
+
+        if the_id in ids:
+            raise ValueError(f"iter {i}: {the_id} already present in set")
+
+        ids.add(the_id)
